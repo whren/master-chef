285fdb22ee471179ecdff6a416d97329b2648a3c
Bertrand Paquet <bpaquet@octo.com>
2012-04-27 16:28:04 +0200

    More ssh

--- tests/vm_driver.rb
+++ tests/vm_driver.rb
@@ -1,11 +1,14 @@
 
+SSH_CONFIG_FILE = File.join(File.dirname(__FILE__), "ssh", "config") 
+SSH_KEY = File.join(File.dirname(__FILE__), "ssh", "id_rsa")
+SSH_OPTS = "-F #{SSH_CONFIG_FILE} -i #{SSH_KEY}"
+%x{chmod 0600 #{SSH_KEY}}
+%x{rm -f /tmp/tmp_known_hosts}
+    
 class VmDriver
 
   def format_chef_ssh cmd
-    config = File.join(File.dirname(__FILE__), "ssh", "config") 
-    ssh_key = File.join(File.dirname(__FILE__), "ssh", "id_rsa")
-    %x{chmod 0600 #{ssh_key} && rm -f /tmp/tmp_known_hosts}
-    "ssh -F #{config} -i #{ssh_key} #{CHEF_USER}@#{ip} #{cmd}"
+    "ssh #{SSH_OPTS} #{CHEF_USER}@#{ip} #{cmd}"
   end
 
   def run cmd
@@ -22,7 +25,7 @@ class VmDriver
 
   def upload_json json
     json_file = File.join(File.dirname(__FILE__), "json", json)
-    exec_local "scp #{json_file} #{CHEF_USER}@#{ip}:/tmp/local.json"
+    exec_local "scp #{SSH_OPTS} #{json_file} #{CHEF_USER}@#{ip}:/tmp/local.json"
     self.run "sudo mv /tmp/local.json /etc/chef/local.json"
   end
 
