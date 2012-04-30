5397c42b9c5f341d6f9fe892be4835a8d5bc8cb1
Bertrand Paquet <bpaquet@octo.com>
2012-04-30 18:09:45 +0200

    Remove comment

--- tests/tests/conf2_test.rb
+++ tests/tests/conf2_test.rb
@@ -6,7 +6,7 @@ class TestJenkins < Test::Unit::TestCase
 
   def test_conf2
     @vm.upload_json "conf2.json"
-    # @vm.run_chef
+    @vm.run_chef
     ok = false
     (1..20).each do |k|
       @http.get 80, "/jenkins/"
